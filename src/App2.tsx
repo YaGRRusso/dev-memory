88f12cde2a6a93bc09b32f7b546ec3574b29fe17
Yago Russo <yagrrusso@gmail.com>
2022-03-26 21:25:00 -0300

    teste

--- src/App2.tsx
+++ src/App2.tsx
@@ -28,94 +28,94 @@ export default function App() {
   const [showing, setShowing] = useState(0)
   const [gridItems, setGridItems] = useState<GridItemType[]>([])
 
-  // // Inicar Jogo
-  // useEffect(() => { handleReset() }, [])
+  // Inicar Jogo
+  useEffect(() => { handleReset() }, [])
 
-  // // Timer
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     if (playing) {
-  //       setTime(time + 1)
-  //     }
-  //   }, 1000)
-  //   return () => clearInterval(timer)
-  // }, [playing, time])
+  // Timer
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (playing) {
+        setTime(time + 1)
+      }
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [playing, time])
 
-  // const handleReset = () => {
-  //   // Resetar Jogo
-  //   setTime(0)
-  //   setMoves(0)
-  //   setShowing(0)
+  const handleReset = () => {
+    // Resetar Jogo
+    setTime(0)
+    setMoves(0)
+    setShowing(0)
 
-  //   // Criar Grid
-  //   let tmpGrid: GridItemType[] = []
-  //   for (let i = 0; i < (gameItems.length * 2); i++) {
-  //     tmpGrid.push({ item: null, shown: false, complete: false })
-  //   }
-  //   for (let x = 0; x < 2; x++) {
-  //     for (let i = 0; i < gameItems.length; i++) {
-  //       let randomPos = -1
-  //       while (randomPos < 0 || tmpGrid[randomPos].item !== null) {
-  //         randomPos = Math.floor(Math.random() * (gameItems.length * 2))
-  //       }
-  //       tmpGrid[randomPos].item = i
-  //     }
-  //   }
-  //   setGridItems(tmpGrid)
+    // Criar Grid
+    let tmpGrid: GridItemType[] = []
+    for (let i = 0; i < (gameItems.length * 2); i++) {
+      tmpGrid.push({ item: null, shown: false, complete: false })
+    }
+    for (let x = 0; x < 2; x++) {
+      for (let i = 0; i < gameItems.length; i++) {
+        let randomPos = -1
+        while (randomPos < 0 || tmpGrid[randomPos].item !== null) {
+          randomPos = Math.floor(Math.random() * (gameItems.length * 2))
+        }
+        tmpGrid[randomPos].item = i
+      }
+    }
+    setGridItems(tmpGrid)
 
-  //   // Reiniciar Jogo
-  //   setPlaying(true)
-  // }
+    // Reiniciar Jogo
+    setPlaying(true)
+  }
 
-  // // Checagem de Cartas
-  // useEffect(() => {
-  //   if (showing === 2) {
-  //     let opened = gridItems.filter(item => item.shown)
-  //     if (opened.length === 2) {
+  // Checagem de Cartas
+  useEffect(() => {
+    if (showing === 2) {
+      let opened = gridItems.filter(item => item.shown)
+      if (opened.length === 2) {
 
-  //       if (opened[0].item === opened[1].item) {
-  //         let tmpGrid = [...gridItems]
-  //         for (let i in tmpGrid) {
-  //           if (tmpGrid[i].shown) {
-  //             tmpGrid[i].complete = true;
-  //             tmpGrid[i].shown = false
-  //           }
-  //         }
-  //         setGridItems(tmpGrid)
-  //         setShowing(0)
-  //       } else {
-  //         setTimeout(() => {
-  //           let tmpGrid = [...gridItems]
-  //           for (let i in tmpGrid) {
-  //             tmpGrid[i].shown = false
-  //           }
-  //           setGridItems(tmpGrid)
-  //           setShowing(0)
-  //         }, 750)
-  //       }
-  //       setMoves(moves + 1)
-  //     }
-  //   }
-  // }, [showing, gridItems])
+        if (opened[0].item === opened[1].item) {
+          let tmpGrid = [...gridItems]
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].complete = true;
+              tmpGrid[i].shown = false
+            }
+          }
+          setGridItems(tmpGrid)
+          setShowing(0)
+        } else {
+          setTimeout(() => {
+            let tmpGrid = [...gridItems]
+            for (let i in tmpGrid) {
+              tmpGrid[i].shown = false
+            }
+            setGridItems(tmpGrid)
+            setShowing(0)
+          }, 750)
+        }
+        setMoves(moves + 1)
+      }
+    }
+  }, [showing, gridItems])
 
-  // // Checar se Venceu
-  // useEffect(() => {
-  //   if (moves > 0 && gridItems.every(item => item.complete)) {
-  //     setPlaying(false)
-  //   }
-  // }, [moves])
+  // Checar se Venceu
+  useEffect(() => {
+    if (moves > 0 && gridItems.every(item => item.complete)) {
+      setPlaying(false)
+    }
+  }, [moves])
 
-  // const handleItemClick = (index: number) => {
-  //   if (playing && index !== null && showing < 2) {
-  //     let tmpGrid = [...gridItems]
+  const handleItemClick = (index: number) => {
+    if (playing && index !== null && showing < 2) {
+      let tmpGrid = [...gridItems]
 
-  //     if (!tmpGrid[index].complete || !tmpGrid[index].shown) {
-  //       tmpGrid[index].shown = true
-  //       setShowing(showing + 1)
-  //     }
-  //     setGridItems(tmpGrid)
-  //   }
-  // }
+      if (!tmpGrid[index].complete || !tmpGrid[index].shown) {
+        tmpGrid[index].shown = true
+        setShowing(showing + 1)
+      }
+      setGridItems(tmpGrid)
+    }
+  }
 
   const teste = () => {
 
